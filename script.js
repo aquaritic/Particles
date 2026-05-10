244f2103410202d6b946dac553ee86fe5a06f8ee
aquaritic <krish.s.lad@gmail.com>
2026-05-09 20:41:14 -0500

    Created the basic framework for the project, made the canvas, styled it to fit with screen displays, and I created the basis for my particle spawning and animations.

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const particles = [];
 
+canvas.width = window.innerWidth;
+canvas.height = canvas.innerHeight;
+
+class Particle {
+    constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
+    this.size = 5;
+    this.color = "black";
+    }
+    update(){
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+};
+
+let mouse = {
+    x: 0,
+    y: 0,
+    down: false
+};
+
+canvas.addEventListener("mousedown", e=> {
+    mouse.down = true;
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+});
+
+canvas.addEventListener("mouseup", () => {
+    mouse.down = false;
+});
+
+canvas.addEventListener("mousemove", e=> {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+});
+
+function animation(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (mouse.down) {
+        particles.push(new Particle(mouse.x, mouse.y));
+    }
+
+    for (let p of particles) {
+        p.update();
+        p.draw();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+animation();
